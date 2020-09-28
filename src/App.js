1221783b962f5864cd1310477beafb6f980d9f0f
satpro <satpro@yandex.ru>
2020-09-28 18:02:53 +0300

    'fuckthemall'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import TodoList from './Todo/TodoList'
 
+
 function App() {
 
     const todos=[
       {id:1,completed:false,title:'купить хлеб'},
       {id:2,completed:false,title:'купить масло'},
       {id:3,completed:false,title:'купить молоко'},
-      {id:4,completed:false,title:'купить колбасы'},
+      {id:4,completed:false,title:'купить колбасы1'},
+      {id:5,completed:false,title:'купить королевских креветок'},
     ];
 
   return (
@@ -16,6 +18,7 @@ function App() {
       <TodoList todos={todos} />
     </div>
   );
+
 }
 
 export default App;
